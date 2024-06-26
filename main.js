1a89ec35e1d9ef9e22af0307a7e0d4e2073b4416
brace <brace@qq.com>
2024-06-26 09:54:36 +0800

    v0.0.1 打包分发

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            // 将此脚本附加到渲染器流程
+            preload: path.join(__dirname, './backed/preload.js')
+        }
+    });
+
+    win.loadFile('./pages/index.html');
+}
+
+// 只有在 app 模块的 ready 事件被激发后才能创建浏览器窗口
+app.whenReady().then(() => {
+    createWindow();
+    console.log("app start ...");
+    // 不能直接在主进程中编辑DOM，因为它无法访问渲染器 文档 上下文 而不是说process对象访问不了
+    // for (const dependency of ['chrome', 'node', 'electron']) {
+    //     console.log(process.versions[dependency]);
+    // }
+
+    // 如果没有窗口打开则打开一个窗口 (macOS)
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    });
+})
+
+// 关闭所有窗口时退出应用
+app.on('window-all-closed', () => {
+    // 如果用户不是在 macOS(darwin) 上运行程序，则调用 app.quit()。
+    if (process.platform !== 'darwin') app.quit();
+})
